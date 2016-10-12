713f4224a410c24435be4bf9eb21cad990c3bfde
Mayank Badola <badola21295@gmail.com>
2016-10-12 18:02:38 +0530

    Added actions for default UI

--- public/client.js
+++ public/client.js
@@ -1,44 +1,87 @@
 const body = document.getElementsByTagName('body')
 
-let account = hoodie.account
-
 // UI Components
-const signInForm = '<div id="sign-in-form" class="input-forms">'
-  + '<h3>Sign In</h3><br>'
-  + '<input name="login-username-field" type="text" placeholder="Username"><br>'
-  + '<input name="login-password-field" type="password" placeholder="Password"><br>'
-  + '<button id="login" class="hoodie-button">SIGN IN</button>'
-  + '</div>'
+const signInForm = '<div id="sign-in-form" class="input-forms">' +
+  '<h3>Sign In</h3><br>' +
+  '<input name="login-username-field" type="text" placeholder="Username"><br>' +
+  '<input name="login-password-field" type="password" placeholder="Password"><br>' +
+  '<button id="login" class="hoodie-button">SIGN IN</button>' +
+  '</div>'
 
-const signUpForm = '<div class="input-forms">'
-  + '<h3>Sign Up</h3><br>'
-  + '<input name="register-username-field" type="text" placeholder="Username"><br>'
-  + '<input name="register-password-field" type="password" placeholder="Password"><br>'
-  + '<button id="register" class="hoodie-button">SIGN UP</button>'
-  + '</div>'
+const signUpForm = '<div class="input-forms">' +
+  '<h3>Sign Up</h3><br>' +
+  '<input name="register-username-field" type="text" placeholder="Username"><br>' +
+  '<input name="register-password-field" type="password" placeholder="Password"><br>' +
+  '<button id="register" class="hoodie-button">SIGN UP</button>' +
+  '</div>'
 
-const pwdResetForm = '<div class="input-forms">'
-  + '<h3>Reset Password</h3><br>'
-  + '<input name="reset-email-field" type="text" placeholder="Email"><br>'
-  + '<button id="reset" class="hoodie-button">REQUEST</button>'
-  + '</div>'
+const pwdResetForm = '<div class="input-forms">' +
+  '<h3>Reset Password</h3><br>' +
+  '<input name="reset-email-field" type="text" placeholder="Email"><br>' +
+  '<button id="reset" class="hoodie-button">REQUEST</button>' +
+  '</div>'
 
 const updateAccount = username => (
-  `<div class="profile-forms">`
-  + `<h3>Account Settings</h3><br>`
-  + `<input name="update-username-field" type="text" placeholder="Username" value="${username}"><br>`
-  + `<input name="update-password-field" type="text" placeholder="Type New Password">`
-  + `<div class="hoodie-tooltip">If you leave it blank, we won't modify the password</div><br>`
-  + `<button id="update" class="hoodie-button">Update Account</button>`
-  + `</div>`
+  `<div class="profile-forms">` +
+  `<h3>Account Settings</h3><br>` +
+  `<input name="update-username-field" type="text" placeholder="Username" value="${username}"><br>` +
+  `<input name="update-password-field" type="text" placeholder="Type New Password">` +
+  `<div class="hoodie-tooltip">If you leave it blank, we won't modify the password</div><br>` +
+  `<button id="update" class="hoodie-button">Update Account</button>` +
+  `</div>`
 )
 
-const deleteAccount = '<div class="profile-forms">'
-  + '<h3>Destroy Account</h3><br>'
-  + '<button id="destroy" class="hoodie-button">Confirm</button>'
-  + '</div>'
+const deleteAccount = '<div class="profile-forms">' +
+  '<h3>Destroy Account</h3><br>' +
+  '<button id="destroy" class="hoodie-button">Confirm</button>' +
+  '</div>'
 
-// Client rendering logic
+const actions = {
+  login: () => {
+    let username = document.querySelector('input[name="login-username-field"]').value
+    let password = document.querySelector('input[name="login-password-field"]').value
+    return account.signIn({username, password})
+  },
+  register: () => {
+    let username = document.querySelector('input[name="register-username-field"]').value
+    let password = document.querySelector('input[name="register-password-field"]').value
+    return account.signUp({username, password})
+  },
+  reset: () => {
+    let contact = document.querySelector('input[name="reset-email-field"]').value
+    return account.request({type: 'passwordreset', contact: contact})
+  },
+  update: () => {
+    let username = document.querySelector('input[name="update-username-field"]').value
+    let password = document.querySelector('input[name="update-password-field"]').value
+    if (password === '') {
+      return account.update({username})
+    } else {
+      return account.update({username, password})
+    }
+  },
+  destroy: () => {
+    return account.destroy()
+  }
+}
+
+// Account instance for hoodie-account-client
+let account = hoodie.account
+
+// Event Listener for Actions
+document.querySelector('body').addEventListener('click', event => {
+  if (actions[event.target.id] !== undefined) {
+    actions[event.target.id]()
+    .then((r) => {
+      console.log(r)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  }
+})
+
+// Client Rendering Logic
 if (account.isSignedIn()) {
   body[0].innerHTML = updateAccount(account.username) + deleteAccount
 } else {
