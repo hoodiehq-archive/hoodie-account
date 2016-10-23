7355ea275e3e434e76f1e5ae0ee0d82c08adee49
Mayank Badola <badola21295@gmail.com>
2016-10-23 11:55:13 +0530

    Separated html and js

--- public/client.js
+++ public/client.js
@@ -1,46 +1,5 @@
 const body = document.querySelector('body')
 
-// UI Components
-const signInForm = '<div id="sign-in-form" class="input-forms">' +
-  '<h3>Sign In</h3><br>' +
-  '<input name="login-username-field" type="text" placeholder="Username"><br>' +
-  '<input name="login-password-field" type="password" placeholder="Password"><br>' +
-  '<button id="login" class="hoodie-button">SIGN IN</button>' +
-  '</div>'
-
-const signUpForm = '<div class="input-forms">' +
-  '<h3>Sign Up</h3><br>' +
-  '<input name="register-username-field" type="text" placeholder="Username"><br>' +
-  '<input name="register-password-field" type="password" placeholder="Password"><br>' +
-  '<button id="register" class="hoodie-button">SIGN UP</button>' +
-  '</div>'
-
-const pwdResetForm = '<div class="input-forms">' +
-  '<h3>Reset Password</h3><br>' +
-  '<input name="reset-email-field" type="text" placeholder="Email"><br>' +
-  '<button id="reset" class="hoodie-button">REQUEST</button>' +
-  '</div>'
-
-const updateAccount = username => (
-  `<div class="profile-forms">` +
-  `<h3>Account Settings</h3><br>` +
-  `<input name="update-username-field" type="text" placeholder="Username" value="${username}"><br>` +
-  `<input name="update-password-field" type="text" placeholder="Type New Password">` +
-  `<div class="hoodie-tooltip">If you leave it blank, we won't modify the password</div><br>` +
-  `<button id="update" class="hoodie-button">Update Account</button>` +
-  `</div>`
-)
-
-const signOutAccount = '<div class="profile-forms">' +
-  '<h3>Sign Out</h3><br>' +
-  '<button id="logout" class="hoodie-button">Confirm</button>' +
-  '</div>'
-
-const deleteAccount = '<div class="profile-forms">' +
-  '<h3>Destroy Account</h3><br>' +
-  '<button id="destroy" class="hoodie-button">Confirm</button>' +
-  '</div>'
-
 const actions = {
   login: () => {
     let username = document.querySelector('input[name="login-username-field"]').value
@@ -82,12 +41,13 @@ document.querySelector('body').addEventListener('click', event => {
     actions[event.target.id]()
     .then((r) => {
       if (event.target.id === 'login') {
-        body.innerHTML = updateAccount(account.username) + signOutAccount + deleteAccount
+        document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'true'))
+        document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'false'))
       } else if (event.target.id === 'register') {
-        body.innerHTML = signInForm + signUpForm + pwdResetForm
         alert('Succesfully registered! Please sign in to access profile settings.')
       } else if (event.target.id === 'destroy' || event.target.id === 'logout') {
-        body.innerHTML = signInForm + signUpForm + pwdResetForm
+        document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'true'))
+        document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'false'))
       }
     })
     .catch((e) => {
@@ -98,7 +58,9 @@ document.querySelector('body').addEventListener('click', event => {
 
 // Client Rendering Logic
 if (account.isSignedIn()) {
-  body.innerHTML = updateAccount(account.username) + signOutAccount + deleteAccount
+  document.querySelector('.generic-loader-wrap').setAttribute('data-hide', 'true')
+  document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'false'))
 } else {
-  body.innerHTML = signInForm + signUpForm + pwdResetForm
+  document.querySelector('.generic-loader-wrap').setAttribute('data-hide', 'true')
+  document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'false'))
 }
