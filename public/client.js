5c2c3c8b7abf1da74c5f896493026bbdcccfb4fa
Gregor <gregor@martynus.net>
2016-11-27 11:59:58 +0100

    avoid arrow functions in code that runs in browser

--- public/client.js
+++ public/client.js
@@ -1,75 +1,84 @@
-const body = document.querySelector('body')
+/* global hoodie */
+'use strict'
 
 // Account instance for hoodie-account-client
-let account = hoodie.account
+var account = hoodie.account
 
-const actions = {
-  login: () => {
-    let username = document.querySelector('input[id="login-username-field"]').value
-    let password = document.querySelector('input[id="login-password-field"]').value
-    return account.signIn({username, password})
+var actions = {
+  login: function login () {
+    var username = document.querySelector('input[id="login-username-field"]').value
+    var password = document.querySelector('input[id="login-password-field"]').value
+    return account.signIn({ username: username, password: password })
   },
-  register: () => {
-    let username = document.querySelector('input[id="register-username-field"]').value
-    let password = document.querySelector('input[id="register-password-field"]').value
-    return account.signUp({username, password})
+  register: function register () {
+    var username = document.querySelector('input[id="register-username-field"]').value
+    var password = document.querySelector('input[id="register-password-field"]').value
+    return account.signUp({ username: username, password: password })
   },
-  reset: () => {
-    let contact = document.querySelector('input[id="reset-email-field"]').value
-    return account.request({type: 'passwordreset', contact: contact})
+  reset: function reset () {
+    var contact = document.querySelector('input[id="reset-email-field"]').value
+    return account.request({ type: 'passwordreset', contact: contact })
   },
-  update: () => {
-    let username = document.querySelector('input[id="update-username-field"]').value
-    let password = document.querySelector('input[id="update-password-field"]').value
+  update: function update () {
+    var username = document.querySelector('input[id="update-username-field"]').value
+    var password = document.querySelector('input[id="update-password-field"]').value
     if (password === '') {
-      return account.update({username})
+      return account.update({ username: username })
     } else {
-      return account.update({username, password})
+      return account.update({ username: username, password: password })
     }
   },
-  logout: () => {
+  logout: function logout () {
     return account.signOut()
   },
-  destroy: () => {
+  destroy: function destroy () {
     return account.destroy()
   }
 }
 
 // Event Listener for Actions
-document.querySelector('body').addEventListener('click', event => {
-  document.querySelectorAll('.messages').forEach(node => node.innerHTML = '')
+document.querySelector('body').addEventListener('click', function (event) {
+  document.querySelectorAll('.messages').forEach(function (node) {
+    node.innerHTML = ''
+  })
   if (actions[event.target.id] !== undefined) {
-    actions[event.target.id]()
-    .then((r) => {
+    actions[event.target.id]().then(function (r) {
       if (event.target.id === 'login') {
-        document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'true'))
-        document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'false'))
+        document.querySelectorAll('.input-forms').forEach(function (node) {
+          return node.setAttribute('data-hide', 'true')
+        })
+        document.querySelectorAll('.profile-forms').forEach(function (node) {
+          return node.setAttribute('data-hide', 'false')
+        })
       } else if (event.target.id === 'register') {
-        let node = document.querySelector('#register-response')
+        var node = document.querySelector('#register-response')
         node.style.color = 'green'
         node.innerHTML = 'Successfully Registration: Please sign in to access profile settings'
       } else if (event.target.id === 'destroy' || event.target.id === 'logout') {
-        document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'true'))
-        document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'false'))
-      } else if (event.target.id === 'update' ) {
-        let node = document.querySelector('#update-response')
-        node.style.color = 'green'
-        node.innerHTML = 'Successful Updation'
+        document.querySelectorAll('.profile-forms').forEach(function (node) {
+          return node.setAttribute('data-hide', 'true')
+        })
+        document.querySelectorAll('.input-forms').forEach(function (node) {
+          return node.setAttribute('data-hide', 'false')
+        })
+      } else if (event.target.id === 'update') {
+        var _node = document.querySelector('#update-response')
+        _node.style.color = 'green'
+        _node.innerHTML = 'Successful Updation'
       }
-    })
-    .catch((e) => {
+    }).catch(function (e) {
       if (event.target.id === 'login') {
-        let node = document.querySelector('#login-response')
+        var node = document.querySelector('#login-response')
         node.style.color = 'red'
         node.innerHTML = e
       } else if (event.target.id === 'register') {
-        let node = document.querySelector('#register-response')
-        node.style.color = 'red'
-        node.innerHTML = e
+        var _node2 = document.querySelector('#register-response')
+        _node2.style.color = 'red'
+        _node2.innerHTML = e
       } else if (event.target.id === 'reset') {
-        let node = document.querySelector('#password-reset-response')
-        node.style.color = 'red'
-        node.innerHTML = e
+        var _node3 = document.querySelector('#password-reset-response')
+        _node3.style.color = 'red'
+        _node3.innerHTML = e
       }
     })
   }
@@ -78,8 +87,12 @@ document.querySelector('body').addEventListener('click', event => {
 // Client Rendering Logic
 if (account.isSignedIn()) {
   document.querySelector('.generic-loader-wrap').setAttribute('data-hide', 'true')
-  document.querySelectorAll('.profile-forms').forEach(node => node.setAttribute('data-hide', 'false'))
+  document.querySelectorAll('.profile-forms').forEach(function (node) {
+    return node.setAttribute('data-hide', 'false')
+  })
 } else {
   document.querySelector('.generic-loader-wrap').setAttribute('data-hide', 'true')
-  document.querySelectorAll('.input-forms').forEach(node => node.setAttribute('data-hide', 'false'))
+  document.querySelectorAll('.input-forms').forEach(function (node) {
+    return node.setAttribute('data-hide', 'false')
+  })
 }
